4a79d753ce2ab2e08ec9bc75f302dc7b8c93f6ee
Dailton Silva <112089713+dailtonccomp@users.noreply.github.com>
2022-12-09 09:01:17 -0300

    jair quase concluido

---
+++ js/custom.js
@@ -0,0 +1,3 @@
+// document.getElementById("imagem").innerHTML = "<a href='img/imagem01.jpg' data-lightbox='mygallery' data-title='2º período'>";
+
+// document.getElementById("imagem").innerHTML += "<img src='img/imagem01.jpg' alt='Foto 01'>";
