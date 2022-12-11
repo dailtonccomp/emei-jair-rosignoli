9bd482ab8acb2048b3084047bcf57eb804d94097
Dailton Silva <112089713+dailtonccomp@users.noreply.github.com>
2022-12-11 17:35:58 -0300

    javascript ok

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
-// document.getElementById("imagem").innerHTML = "<a href='img/imagem01.jpg' data-lightbox='mygallery' data-title='2º período'>";
 
-// document.getElementById("imagem").innerHTML += "<img src='img/imagem01.jpg' alt='Foto 01'>";
+for(var i = 1; i<=20; i++) {    
+    if(i<10) {
+        document.getElementById("imagem").innerHTML += "<a href='img/imagem0"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/imagem0"+i+".jpg' alt='Foto 0"+i+"'>";      
+    }else {
+        document.getElementById("imagem").innerHTML += "<a href='img/imagem"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/imagem"+i+".jpg' alt='Foto "+i+"'>";
+    }
+    document.getElementById("imagem").innerHTML += "</a>";
+}
