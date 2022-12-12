69e36939ed5a0bc53d7629e367c3fdacebbfac66
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-12-12 15:30:03 -0300

    fotos organizadas

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ for(var i = 1; i<=18; i++) {
         document.getElementById("imagem1").innerHTML += "<a href='img/periodo1/"+i+".jpg' data-lightbox='mygallery' data-title='1º período'><img src='img/periodo1/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
 
-for(var i = 1; i<=113; i++) {    
+for(var i = 1; i<=100; i++) {    
     
     document.getElementById("imagem2").innerHTML += "<a href='img/periodo2/"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/periodo2/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
