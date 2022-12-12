eae8c01c9d77dbc21f2cab3b2fc92cb3937ff5b1
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-12-12 15:41:45 -0300

    ultima atualização

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-for(var i = 1; i<=18; i++) {    
+for(var i = 1; i<=26; i++) {    
     
         document.getElementById("imagem1").innerHTML += "<a href='img/periodo1/"+i+".jpg' data-lightbox='mygallery' data-title='1º período'><img src='img/periodo1/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
