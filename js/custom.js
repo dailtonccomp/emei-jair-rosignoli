049302487dfc05348d90fa1e10b2351bb1851033
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-12-12 16:05:51 -0300

    atualizando01

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ for(var i = 1; i<=100; i++) {
     document.getElementById("imagem2").innerHTML += "<a href='img/periodo2/"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/periodo2/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
 
-for(var i = 1; i<=6; i++) {    
+for(var i = 1; i<=22; i++) {    
     
     document.getElementById("imagem3").innerHTML += "<a href='img/africa/"+i+".jpg' data-lightbox='mygallery' data-title='II Edição do Projeto Africanidades'><img src='img/africa/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
